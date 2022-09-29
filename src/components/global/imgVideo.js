2c4227afe295a92ed257cf4411de7c26b03a259a
Hadi Muhammed <dodehadi@gmail.com>
2022-09-29 15:46:07 +0300

    mute video

--- src/components/global/imgVideo.js
+++ src/components/global/imgVideo.js
@@ -9,7 +9,7 @@ const ImageVideo = () => {
     return ( 
         <>
             {/* <video id={isPlay ? "" : "none"} src={airport} autoPlay loop muted /> */}
-            <iframe id={isPlay ? "" : "none"}  src="https://www.youtube.com/embed/m8n2o3Qx504?autoplay=1"/>
+            <iframe id={isPlay ? "" : "none"}  src="https://www.youtube.com/embed/m8n2o3Qx504?autoplay=1&mute=1"/>
             <img id={isPlay ? "none" : ""} src={image} alt="image" />
             <img id={isPlay ? "none" : ""} src={playIcon} alt="playIcon" onClick={() => setIsPlay(true)}/>
         </>
